e137c4a752e72fbb1a8ea33c7f14df1915e83c73
KENTA62225 <97034496+KENTA62225@users.noreply.github.com>
2023-06-17 19:34:55 +0900

    Update undeploy.js

--- undeploy.js
+++ undeploy.js
@@ -1,8 +1,8 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
+const { clientId, guildId, token } = require('./config.json');
 
 const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationCommands(clientId), { body: [] })
-	.then(() => console.log('Successfully deleted all application commands.'))
+	.then(() => console.log('すべてのスラッシュコマンドの登録解除が正常に完了しました。'))
 	.catch(console.error);
