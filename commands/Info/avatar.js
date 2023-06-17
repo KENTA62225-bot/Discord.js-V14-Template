c9c17224c7604973563ac11339094deaa86dc63d
KENTA62225 <97034496+KENTA62225@users.noreply.github.com>
2023-06-17 19:50:15 +0900

    Update avatar.js

--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -5,23 +5,23 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('avatar')
 		.setDescription('ユーザーのアバターを取得')
-        .addUserOption(option => 
+	        .addUserOption(option => 
 			option
 			.setName('target')
 			.setDescription('アバターを取得する人')
-            .setRequired(true)
+	                .setRequired(true)
 		)
 		.setDMPermission(false),
 
 	async execute(interaction) {
-        const user = interaction.options.getMember('target');
+		const user = interaction.options.getMember('target');
 
 		const embed = new EmbedBuilder()
 		.setTitle('> Avatar')
-        .setImage(user.displayAvatarURL())
+		.setImage(user.displayAvatarURL())
 		.setFooter({ text: `${interaction.client.user.tag}`, iconURL: `${interaction.client.user.displayAvatarURL()}`})
 		.setTimestamp()
-        .setColor(color);
+		.setColor(color);
 
 		return interaction.reply({ embeds: [embed]})
 	}
