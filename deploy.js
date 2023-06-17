6a7a11aaf5c635951b8fc02e1f730a4f302ff95f
KENTA62225 <97034496+KENTA62225@users.noreply.github.com>
2023-06-17 19:34:24 +0900

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
+const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 
 const commands = [];
@@ -17,14 +17,14 @@ const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`${commands.length} 個のスラッシュコマンドの登録を開始しました。 | ${new Date().toLocaleString('js-JP', { timeZone: 'Asia/Tokyo'})}`);
 
 		const data = await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		console.log(`${data.length} 個のスラッシュコマンドの登録が正常に完了しました。 | ${new Date().toLocaleString('js-JP', { timeZone: 'Asia/Tokyo'})}`);
 	} catch (error) {
 		console.error(error);
 	}
