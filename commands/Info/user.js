d114c6486a14b61ecdd382a22a169f7c753e2e16
KENTA62225 <97034496+KENTA62225@users.noreply.github.com>
2023-06-17 19:51:35 +0900

    Update user.js

--- commands/Info/user.js
+++ commands/Info/user.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { color, ownerid, owneridsub } = require('../../config.json');
-
+const { color } = require('../../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -21,7 +20,7 @@ module.exports = {
 		.setTitle(`> User Info`)
 		.setDescription(`【 **__${member.user.tag}__** 】`)
 		.setThumbnail(member.user.displayAvatarURL())
-	    .setFields(
+		.setFields(
 			{ name: `-ユーザーID`, value: `${member.user.id}`},
 			{ name: `-アカウント作成日`, value: `${member.user.createdAt.toLocaleString()}`},
 			{ name: `-参加日`, value: `${member.joinedAt.toLocaleString()}`},
@@ -29,7 +28,7 @@ module.exports = {
 		)
 		.setFooter({ text: `${interaction.client.user.tag}`, iconURL: `${interaction.client.user.displayAvatarURL()}`})
 		.setTimestamp()
-	    .setColor(color);
+	        .setColor(color);
 
 		return interaction.reply({ embeds: [embed] });
 	},
