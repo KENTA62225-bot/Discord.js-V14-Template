a172b07ccd093c2f97a92425c8abe8cd30757eb9
KENTA62225 <97034496+KENTA62225@users.noreply.github.com>
2023-08-12 16:19:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ const client = new Client({
 client.commands = new Collection();
 const commandFolders = fs.readdirSync('./commands');
 for (const folder of commandFolders) {
-  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-  for (const file of commandFiles) {
-    const command = require(`./commands/${folder}/${file}`);
-    client.commands.set(command.data.name, command);
-  }
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.data.name, command);
+    }
 }
 
 //eventファイル読み込み
